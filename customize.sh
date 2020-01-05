3b909791bc888f9256340418e9c01658ac5821dd
gursewak10 <45232955+gursewak10@users.noreply.github.com>
2020-01-05 19:38:33 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,1 +1,8 @@
+# Variables
+DEVICE=`getprop ro.product.device`
+ui_print "Device: "$DEVICE
+# Device check
+if [ $DEVICE = "violet" ] && [ $DEVICE = "Violet" ] && [ $DEVICE = "whyred" ] && [ $DEVICE = "Whyred" ]; then
+return 0;
+fi
 
