c621847245303b81b2805aebc6b82886af7fa2fc
gursewak10 <45232955+gursewak10@users.noreply.github.com>
2020-01-06 11:21:23 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,18 @@
-# Variables
-DEVICE=`getprop ro.product.device`
-ui_print "Device: "$DEVICE
-# Device check
-if [ $DEVICE = "violet" ] && [ $DEVICE = "Violet" ] && [ $DEVICE = "whyred" ] && [ $DEVICE = "Whyred" ]; then
-return 0;
-fi
+ui_print "______________________________"
+ui_print " "
+ui_print "ultra chck"
 
+DEVICE=`getprop ro.product.device`
+RELEASE=`getprop ro.build.version.release`
+SECURITY_PATCH_VERSION=`getprop ro.build.version.security_patch`
+ui_print " "
+
+
+ui_print "Device: "$DEVICE
+ui_print "Android version: "$RELEASE
+ui_print "Security patch version: "$SECURITY_PATCH_VERSION
+ui_print " "
+
+if [ $DEVICE != "violet" ] && [ $DEVICE != "whyred" ]; then
+  abort " => Device '"$DEVICE"' is not supported"
+fi
