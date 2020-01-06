db725f25fde5f9b7795634b3687a7c5087620cb2
gursewak10 <45232955+gursewak10@users.noreply.github.com>
2020-01-06 11:28:33 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,16 +1,6 @@
-ui_print "______________________________"
-ui_print " "
-ui_print "ultra chck"
-
 DEVICE=`getprop ro.product.device`
-RELEASE=`getprop ro.build.version.release`
-SECURITY_PATCH_VERSION=`getprop ro.build.version.security_patch`
-ui_print " "
-
 
 ui_print "Device: "$DEVICE
-ui_print "Android version: "$RELEASE
-ui_print "Security patch version: "$SECURITY_PATCH_VERSION
 ui_print " "
 
 if [ $DEVICE != "viole" ] && [ $DEVICE != "whyred" ]; then
