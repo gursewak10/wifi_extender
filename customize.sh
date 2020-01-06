a6ca677009c2dac0e7bed4512c29eca626b25584
gursewak10 <45232955+gursewak10@users.noreply.github.com>
2020-01-06 11:29:39 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,6 @@
 DEVICE=`getprop ro.product.device`
-
 ui_print "Device: "$DEVICE
 ui_print " "
-
 if [ $DEVICE != "viole" ] && [ $DEVICE != "whyred" ]; then
   abort " => Device '"$DEVICE"' is not supported"
 fi
